d1dba4740cca00474ff1106fb0cfe75fd3cf68e6
Antonov333 <antonovsergei333@yahoo.com>
2023-04-24 13:20:14 +0300

    Added homework source code

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Course 2, lesson 3: OOP, part 2");
-
-    }
-}
---
+++ src/course2_lesson3_OOP_p2/Bicycle.java
@@ -0,0 +1,11 @@
+package course2_lesson3_OOP_p2;
+
+public class Bicycle {
+
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
